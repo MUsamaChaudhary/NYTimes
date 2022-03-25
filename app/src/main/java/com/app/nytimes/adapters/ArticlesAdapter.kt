0defc99b381f6b2e3338f3109cc94a9e953b0cc7
usama-esotec <usama98ch@gmail.com>
2022-03-25 15:41:08 +0500

    basic finishing

--- app/src/main/java/com/app/nytimes/adapters/ArticlesAdapter.kt
+++ app/src/main/java/com/app/nytimes/adapters/ArticlesAdapter.kt
@@ -10,15 +10,16 @@ import com.app.nytimes.R
 import com.app.nytimes.databinding.LayoutArticlesItemBinding
 
 import com.app.nytimes.models.MostViewedArticle
+import com.app.nytimes.utils.Utils
 
-class ArticlesAdapter (
+class ArticlesAdapter(
     private var context: Context,
     private var articles: ArrayList<MostViewedArticle>,
-    private var listener: ClickListener
-) : RecyclerView.Adapter<ArticlesAdapter.ViewHolder>()  {
+    private var listener: ClickListener,
+) : RecyclerView.Adapter<ArticlesAdapter.ViewHolder>() {
 
     companion object {
-        private  val TAG = ArticlesAdapter::class.simpleName
+        private val TAG = ArticlesAdapter::class.simpleName
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
@@ -29,40 +30,40 @@ class ArticlesAdapter (
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val article = articles[position]
         // set title
-        article?.title?.let{
-            holder.binding.articleTitle.visibility=View.VISIBLE
+        article?.title?.let {
+            Utils.setViewVisibility(holder.binding.articleTitle, null, View.VISIBLE)
             holder.binding.articleTitle.text = it
         } ?: run {
-            holder.binding.articleTitle.visibility=View.GONE
+            Utils.setViewVisibility(holder.binding.articleTitle, null, View.GONE)
         }
         // set source
-        article?.source?.let{
-            holder.binding.articleSource.visibility=View.VISIBLE
+        article?.source?.let {
+            Utils.setViewVisibility(holder.binding.articleSource, null, View.VISIBLE)
             holder.binding.articleSource.text = it
-        }?: run {
-            holder.binding.articleSource.visibility=View.GONE
+        } ?: run {
+            Utils.setViewVisibility(holder.binding.articleSource, null, View.GONE)
         }
         // set section
-        article?.section?.let{
-            holder.binding.articleSections.visibility=View.VISIBLE
+        article?.section?.let {
+            Utils.setViewVisibility(holder.binding.articleSections, null, View.VISIBLE)
             holder.binding.articleSections.text = it
-        }?: run {
-            holder.binding.articleSections.visibility=View.GONE
+        } ?: run {
+            Utils.setViewVisibility(holder.binding.articleSections, null, View.GONE)
         }
         // set section
-        article?.published_date?.let{
-            holder.binding.timestamp.visibility=View.VISIBLE
+        article?.published_date?.let {
+            Utils.setViewVisibility(holder.binding.timestamp, null, View.VISIBLE)
             holder.binding.timestamp.text = it
-        }?: run {
-            holder.binding.timestamp.visibility=View.GONE
+        } ?: run {
+            Utils.setViewVisibility(holder.binding.timestamp, null, View.GONE)
         }
 
         // handle on click listner on article list items
         holder.binding.root.setOnClickListener {
-            listener.onViewClick(article,position,it)
+            listener.onViewClick(article, position, it)
         }
         holder.binding.detail.setOnClickListener {
-            listener.onViewClick(article,position,it)
+            listener.onViewClick(article, position, it)
         }
 
     }
@@ -72,6 +73,7 @@ class ArticlesAdapter (
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val binding = LayoutArticlesItemBinding.bind(itemView)
     }
+
     interface ClickListener {
         fun onViewClick(articleData: MostViewedArticle, position: Int, view: View)
     }
